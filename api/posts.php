e123cb0f01ec19e99780b0260bbd694b4615d98a
Fabian Kirchmann <72859811+bbs2h20aki@users.noreply.github.com>
2022-02-11 14:14:52 +0100

    upadtes deleting unused code

--- api/posts.php
+++ api/posts.php
@@ -15,11 +15,11 @@ if ($method == 'GET') {
 } elseif ($method == 'POST') {
     if ($_POST != null && !$id) {
         extract($_POST);
-        DB::query("INSERT INTO $tableName VALUES(null, :title, :body, :author, null)", array(':title' => $title, ':body' => $body, ':author' => $author));
+        DB::query("INSERT INTO $tableName VALUES(null, :title, :body)", array(':title' => $title, ':body' => $body));
         $data = DB::query("SELECT * FROM $tableName ORDER BY id DESC LIMIT 1");
         echo json_encode(['message' => 'Post added to the database successfully.', 'success' => true, 'post' => $data[0]]);
     } else {
-        echo json_encode(['message' => 'Please pill in all the credentials', 'success' => false]);
+        echo json_encode(['message' => 'Please fill in all the credentials', 'success' => false]);
     }
 } elseif ($id) {
     $post = DB::query("SELECT * FROM $tableName WHERE id=:id", array(':id' => $id));
@@ -27,7 +27,7 @@ if ($method == 'GET') {
         if ($method == 'PUT') {
             extract(json_decode(file_get_contents('php://input'), true));
             // Update the Post in the Database
-            DB::query("UPDATE $tableName SET title=:title, body=:body, author=:author WHERE id = :id", array(':title' => $title, ':body' => $body, ':author' => $author, ':id' => $id));
+            DB::query("UPDATE $tableName SET title=:title, body=:body WHERE id = :id", array(':title' => $title, ':body' => $body, ':id' => $id));
             $data = DB::query("SELECT * FROM $tableName WHERE id=:id", array(':id' => $id));
             echo json_encode(['post' => $data[0], 'message' => 'Post Updated successfully', 'success' => true]);
         } elseif ($method == 'DELETE') {
