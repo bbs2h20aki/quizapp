16cdd2b06c955e134dba4553448b44be1f6cc55f
Fabian Kirchmann <72859811+bbs2h20aki@users.noreply.github.com>
2022-02-11 14:27:16 +0100

    Update index.php

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ $method = $_SERVER['REQUEST_METHOD'];
 $request_uri = $_SERVER['REQUEST_URI'];
    //echo $request_uri;
 
-$tables = ['Request'];
+$tables = ['REQUEST'];
 $url = rtrim($request_uri, '/');
 $url = filter_var($request_uri, FILTER_SANITIZE_URL);
 $url = explode('/', $url);
